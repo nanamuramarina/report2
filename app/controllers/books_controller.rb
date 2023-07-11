2b20dc6b53aae3815a1ad0808714767edec59996
EC2 Default User <ec2-user@ip-172-31-3-23.ap-northeast-1.compute.internal>
2023-07-11 13:37:32 +0000

    編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,44 @@
 class BooksController < ApplicationController
+  module ApplicationHelper
+  def full_title(page_title='WebCampBackendNew')
+    base_title="WebCampBackendNew"
+    if page_title.empty?
+      base_title
+     else
+      "#{ page_title } | #{ base_title}"
+     end
+    end
+   end
+
+
   def new
     @book = Book.new
   end
 
   def index
     @books = Book.all
+    @books = Book.all.order(created_at: :desc)
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
 
   def show
     @book = Book.find(params[:id])
+
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -30,7 +47,7 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    Book.update(book_paraams)
+    Book.update(book_params)
     redirect_to book_path(book.id)
   end
 
